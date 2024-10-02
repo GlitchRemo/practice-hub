41fca746329cd8d69647c8b62289027b02bb157b
ghosar_grid <riya.ghosal@nationalgrid.com>
2024-10-03 00:12:54 +0530

    Add. Producer & Consumer service

---
+++ kafka/Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Confluent.Kafka;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConsumerConfig()
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "riya-consumer-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("riya-topic");
+var cts = new CancellationTokenSource();
+
+while (true)
+{
+    var message = consumer.Consume(cts.Token);
+    Console.WriteLine($"Consumed message '{message.Message.Value}' at: '{message.TopicPartitionOffset}'.");
+}
--- kafka/Producer/Program.cs
+++ kafka/Producer/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Confluent.Kafka;
+
+var config = new ProducerConfig()
+{
+    BootstrapServers = "localhost:9092",
+    ClientId = "riya-producer"
+};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    var result = await producer.ProduceAsync("riya-topic", new Message<Null, string>()
+    {
+        Value = "Ami je tomar"
+    });
+
+    Console.WriteLine($"Message produces to {result.TopicPartitionOffset}");
+}
+catch (ProduceException<Null, string> e)
+{
+    Console.WriteLine($"Error while producing message: {e.Message}, {e.Error.Code}");
+}
